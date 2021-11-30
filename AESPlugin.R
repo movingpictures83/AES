1858f0e1a14fdbd07c7f98f9fc94a9140714f2ac
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-11-30 11:34:08 -0500

    Initial

---
+++ AESPlugin.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+library(ggrepel)
+library(cowplot)
+library(plspm)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <- paste(pfix, "/", sep="")
+  }
+
+mydata <- read.csv(paste(pfix, parameters["csvfile", 2], sep="/"))
+mydata <- as.data.frame(mydata)
+numeric_vars <- readLines(paste(pfix, parameters["features", 2], sep="/"))
+
+mydata_num <<- mydata[, numeric_vars]
+mydata_num$classification <<- "normal"
+
+parameters_cats <<- read.table(paste(pfix, parameters["categories", 2], sep="/"), as.is=T);
+cats <- c()
+for (i in 1:nrow(parameters_cats)) {
+   cat_samples <- readLines(paste(pfix, parameters_cats[i,2], sep="/"))
+   cats <- c(cats, cat_samples)
+   mydata_num$classification[match(cat_samples, rownames(mydata_num))] <<- parameters_cats[i,1]
+}
+
+mydata_num_sub <<- match(
+  cats,
+  rownames(mydata_num)
+)
+
+mydata_num$label <<- ""
+mydata_num$label[mydata_num_sub] <<- rownames(mydata_num)[mydata_num_sub]
+
+thecols <<- readLines(paste(pfix, parameters["columns", 2], sep="/"))
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+library(GGally)
+yy <<- ggpairs(mydata_num,
+  columns = thecols,
+  ggplot2::aes(colour = classification)
+)
+print(yy)
+#print(str(yy))
+write.csv(yy$label, paste(outputfile, "csv", sep="."))
+ggsave(outputfile)
+}
